7766bf6800ee20511f833d7098fcb3b5d8890710
Marco Leong <leong.chou.kin@usj.edu.mo>
2012-12-07 18:24:06 +0800

    revert apache2 changes

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -6,7 +6,7 @@ class apache {
 
   # Make sure apache is running
   service {'apache2':
-    ensure  => stopped,
+    ensure  => running,
     # Make sure apache is installed before checking
     require => Package['apache2'],
   }
