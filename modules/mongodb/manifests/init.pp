b55a4efcba3ece9d087638ba54d355ea4a7bde44
Guill Lo <guill.bout@gmail.com>
2012-12-11 15:32:11 +0800

    Make mongodb databases available from the host
    Host port (forwarded) is 27018

--- modules/mongodb/manifests/init.pp
+++ modules/mongodb/manifests/init.pp
@@ -3,6 +3,13 @@ class mongodb {
         ensure => '1:2.0.4-1ubuntu2',
     }
 
+    # Make sure mongodb is running
+    service {'mongodb':
+        ensure  => running,
+        # Make sure mongodb is installed before checking
+        require => Package['mongodb'],
+    }
+
     # Install PHP driver for mongo
     exec { 'php-mongo-driver':
         command => 'sudo pecl install mongo',
@@ -13,12 +20,23 @@ class mongodb {
 
     # Enable PHP driver for mongo
     file {'mongo-driver-enable':
-    ensure  => present,
-    path    => '/etc/php5/conf.d/mongo-driver-enable.ini',
-    owner   => 'root',
-    group   => 'root',
-    content => 'extension=mongo.so',
-    require => [ Package['apache2'], Package['php5'] ],
-    notify  => Service['apache2'],
-  }
+        ensure  => present,
+        path    => '/etc/php5/conf.d/mongo-driver-enable.ini',
+        owner   => 'root',
+        group   => 'root',
+        content => 'extension=mongo.so',
+        require => [ Package['apache2'], Package['php5'] ],
+        notify  => Service['apache2'],
+    }
+
+    # mongodb.conf file, allows connection from host
+    file {'mongodb-conf':
+        ensure  => present,
+        path    => '/etc/mongodb.conf',
+        owner   => 'root',
+        group   => 'root',
+        content => template('mongodb/mongodb.conf.erb'),
+        require => Package['mongodb'],
+        notify  => Service['mongodb'],
+    }
 }
